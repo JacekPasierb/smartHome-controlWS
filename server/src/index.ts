76183a47b65dfcf2a5b000bc52094880b26bd655
Jacek Pasierb <jack_byk@o2.pl>
2026-04-15 21:25:44 +0200

    Feature - backend deployed on Render

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://smarthome-frontend.netlify.app/",
   },
 });
 
